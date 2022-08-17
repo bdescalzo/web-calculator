a20008258089c162faa3ae46920b8c06c3548120
Beñat Descalzo Alcuaz <guaibenat@gmail.com>
2022-08-17 18:51:13 +0200

    Add basic display behaviour

--- js/app.js
+++ js/app.js
@@ -1,6 +1,24 @@
+// Grab elements from DOM
 const numberButtons = document.querySelectorAll(".number-button");
 const operatorButtons = document.querySelectorAll(".operator-button");
 const displayEl = document.querySelector(".display");
+
+let currentNumber = " ";
+let result = 0;
+let currentOperator;
+
+// Each number button should change the number currently being used and force the display to update
+numberButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    currentNumber += e.target.value;
+    updateDisplay();
+  });
+});
+
+function updateDisplay() {
+  expression = currentNumber;
+  displayEl.textContent = expression;
+}
 
 // Math functions
 function add(a, b) {
